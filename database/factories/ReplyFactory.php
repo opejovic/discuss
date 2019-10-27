0fc2ad0c9563913d1ae50d375d3e2fb146205863
Ognjen <opejovic@gmail.com>
2019-10-27 21:47:51 +0100

    Install laravel and add models, migrations and factories

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
+use App\Reply;
+use Faker\Generator as Faker;
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'thread_id' => function () {
+            return factory(Thread::class)->create()->id;
+        },
+        'body' => $faker->paragraph
+    ];
+});
---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
+use App\Thread;
+use Faker\Generator as Faker;
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
+        'title' => $faker->title,
+        'body' => $faker->paragraph
+    ];
+});

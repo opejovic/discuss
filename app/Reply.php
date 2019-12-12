22d91b54c4473913cdc33f57940e43b8f3fcd9eb
Ognjen <opejovic@gmail.com>
2019-12-12 16:46:27 +0100

    Make mentioned users name blue.

--- app/Reply.php
+++ app/Reply.php
@@ -48,11 +48,11 @@ class Reply extends Model
         // as the usernames can contain '-'.
         // Example: "Hey @Jane-Doe!" will return "Jane-Doe",
         // which is the users name, and it will get that user.
-        
+
         preg_match_all('/@([\w\-]+)/', $this->body, $matches);
 
         $mentionedNames = collect($matches[1]);
-        
+
         return User::whereIn('name', $mentionedNames)->get();
     }
 
@@ -66,6 +66,6 @@ class Reply extends Model
     {
         // $1 - Matching name without the @ symbol.
         // $0 - Matching name with the @ symbol.
-        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a href="/profiles/$1">$0</a>', $body);
+        $this->attributes['body'] = preg_replace('/@([\w\-]+)/', '<a class="text-blue-400" href="/profiles/$1">$0</a>', $body);
     }
 }
